4c8badc8c97fa1ff445b1f9b9d66f85f06e3a1a5
Yaroslav <andrexvich@icloud.com>
2024-10-07 21:33:02 +0300

    add @media

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -5,6 +5,11 @@ export default function AppHeader() {
     return(
         <header className={styles.headerContainer}>
             <img src={headerLogo} alt="logo"/>
+            <div>
+                <h2 className={styles.headerTitle}>Учавствуй Совершенствуйся
+                    Выигрывай бабло</h2>
+                <p className={styles.headerDesc}>последнее не точно</p>
+            </div>
         </header>
     )
 }
