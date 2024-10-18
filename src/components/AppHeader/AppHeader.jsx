64df776992710f5efd06080ad19eef72d24cfca8
Yaroslav <andrexvich@icloud.com>
2024-10-18 17:55:17 +0300

    create style default

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -1,6 +1,7 @@
 import s from './AppHeader.module.css'
 
 export default function AppHeader() {
+    console.log(s)
     return (
         <header className={s.header}>
             <div className={s.wrapper}></div>
