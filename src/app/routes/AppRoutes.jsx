2a4f3419570cb84f2904a8b7cf36f0f53bd01d36
Yaroslav <andrexvich@icloud.com>
2024-10-18 22:39:06 +0300

    update AppProfile

--- src/app/routes/AppRoutes.jsx
+++ src/app/routes/AppRoutes.jsx
@@ -7,17 +7,16 @@ import {RoutesList} from "./routes.types.js";
 import Preloader from "../../components/Preloader/Preloader.jsx";
 import AppLayout from "../AppLayout.jsx";
 
-const Profile = lazy(() => import("./../../components/AppProfile//AppProfile.jsx"));
+const AppProfile = lazy(() => import("../../components/AppProfile/AppProfile.jsx"));
 
 const AppRoutes = () => {
     return (
         <Suspense fallback={<Preloader/>}>
             <Routes>
+                <Route path={RoutesList.profile} element={AppProfile}></Route>
                 <Route path={RoutesList.home} element={<AppLayout/>}>
                     {/*=== home page ===*/}
-                    <Route index element={<Profile/>}/>
                 </Route>
-
             </Routes>
         </Suspense>
     );
--- src/app/routes/routes.types.js
+++ src/app/routes/routes.types.js
@@ -1,4 +1,4 @@
 export const RoutesList = {
     home: "/",
-    admin: "/admin",
+    profile: "/profile",
 };
