76c2acfae95db213f0e061193e037ed274b10ad6
Yaroslav <andrexvich@icloud.com>
2024-10-24 09:01:02 +0300

    add method map for AppProfile.jsx

--- src/components/AppProfile/AppProfile.jsx
+++ src/components/AppProfile/AppProfile.jsx
@@ -4,56 +4,64 @@ import Badge from "../Badge/Badge";
 import icon from "../../assets/icons/ellipse.svg";
 
 const badgesList = ["Дизайн", "Data Science", "AR"];
+const cardsData = Array(6).fill({
+  title: "More Tech",
+  date: "20 октября 2021",
+  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  statuses: [
+    "Есть отбор",
+    "Регистрация до 21.09"
+  ]
+});
 
 const AppProfile = () => {
   return (
-    <div className={s.appProfile}>
-      <div className={s.container}>
-        <div className={s.wrapper}>
-          <div className={s.card}>
-            <div className={s.cardWrapper}>
-              <div className={s.itemTop}>
-                <h5 className={s.cardTitle}>
-                  More Tech <br />
-                  <span className={s.cardDate}>20 октября 2021</span>
-                </h5>
-                <a href="#" className={s.link}>
-                  Сайт
-                </a>
-              </div>
-              <div className={s.itemBottom}>
-                <div className={s.badges}>
-                  {badgesList.map((title, index) => {
-                    return <Badge badgeTitle={title} key={index} />;
-                  })}
-                </div>
-                <div className={s.descriptions}>
-                  <p className={s.description}>
-                    Lorem ipsum dolor sit amet, consectetur <br /> adipiscing
-                    elit. Odio amet feugiat ut integer <br /> tincidunt sed.
-                    Fusce vulputate sed commodo, <br /> sed lorem. Mi semper
-                    orci, semper vestibulum.
-                  </p>
-                </div>
-                <div className={s.statuses}>
-                  <div className={s.statusItem}>
-                    <img src={icon} alt="icon" />
-                    <span className={s.status}> Есть отбор</span>
+      <div className={s.appProfile}>
+        <div className={s.container}>
+          <div className={s.wrapper}>
+            <div className={s.cardGrid}>
+              {cardsData.map((card, index) => (
+                  <div className={s.card} key={index}>
+                    <div className={s.cardWrapper}>
+                      <div className={s.itemTop}>
+                        <h5 className={s.cardTitle}>
+                          {card.title} <br />
+                          <span className={s.cardDate}>{card.date}</span>
+                        </h5>
+                        <a href="#" className={s.link}>
+                          Сайт
+                        </a>
+                      </div>
+                      <div className={s.itemBottom}>
+                        <div className={s.badges}>
+                          {badgesList.map((title, index) => (
+                              <Badge badgeTitle={title} key={index} />
+                          ))}
+                        </div>
+                        <div className={s.descriptions}>
+                          <p className={s.description}>
+                            {card.description}
+                          </p>
+                        </div>
+                        <div className={s.statuses}>
+                          {card.statuses.map((status, statusIndex) => (
+                              <div className={s.statusItem} key={statusIndex}>
+                                <img src={icon} alt="icon" />
+                                <span className={s.status}>{status}</span>
+                              </div>
+                          ))}
+                        </div>
+                      </div>
+                    </div>
                   </div>
-                  <div className={s.statusItem}>
-                    <img src={icon} alt="icon" />
-                    <span className={s.status}>Регистрация до 21.09</span>
-                  </div>
-                </div>
-              </div>
+              ))}
             </div>
           </div>
         </div>
-      </div>
 
-      {/* Змінний контент буде рендеритись тут */}
-      <Outlet />
-    </div>
+        {/* Змінний контент буде рендеритись тут */}
+        <Outlet />
+      </div>
   );
 };
 
