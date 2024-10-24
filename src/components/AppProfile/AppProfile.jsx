0a22e6563a13f4fdc9f764b6eacb03f78b716abe
KILL0real <divannijexpert@gmail.com>
2024-10-24 14:00:43 +0300

    Fix media query in Header, add media query in Profile and wrapping cards

--- src/components/AppProfile/AppProfile.jsx
+++ src/components/AppProfile/AppProfile.jsx
@@ -4,64 +4,61 @@ import Badge from "../Badge/Badge";
 import icon from "../../assets/icons/ellipse.svg";
 
 const badgesList = ["Дизайн", "Data Science", "AR"];
+
 const cardsData = Array(6).fill({
   title: "More Tech",
   date: "20 октября 2021",
-  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-  statuses: [
-    "Есть отбор",
-    "Регистрация до 21.09"
-  ]
+  description:
+    "Lorem ipsum dolor sit amet, consectetur \n adipiscing elit. Odio amet feugiat ut integer \n tincidunt sed. Fusce vulputate sed commodo, \n sed lorem. Mi semper orci, semper vestibulum.",
+  statuses: ["Есть отбор", "Регистрация до 21.09"],
 });
 
 const AppProfile = () => {
   return (
-      <div className={s.appProfile}>
-        <div className={s.container}>
-          <div className={s.wrapper}>
-            <div className={s.cardGrid}>
-              {cardsData.map((card, index) => (
-                  <div className={s.card} key={index}>
-                    <div className={s.cardWrapper}>
-                      <div className={s.itemTop}>
-                        <h5 className={s.cardTitle}>
-                          {card.title} <br />
-                          <span className={s.cardDate}>{card.date}</span>
-                        </h5>
-                        <a href="#" className={s.link}>
-                          Сайт
-                        </a>
-                      </div>
-                      <div className={s.itemBottom}>
-                        <div className={s.badges}>
-                          {badgesList.map((title, index) => (
-                              <Badge badgeTitle={title} key={index} />
-                          ))}
+    <div className={s.appProfile}>
+      <div className={s.container}>
+        <div className={s.wrapper}>
+          <div className={s.cardGrid}>
+            {cardsData.map((card, index) => (
+              <div className={s.card} key={index}>
+                <div className={s.cardWrapper}>
+                  <div className={s.itemTop}>
+                    <h5 className={s.cardTitle}>
+                      {card.title} <br />
+                      <span className={s.cardDate}>{card.date}</span>
+                    </h5>
+                    <a href="#" className={s.link}>
+                      Сайт
+                    </a>
+                  </div>
+                  <div className={s.itemBottom}>
+                    <div className={s.badges}>
+                      {badgesList.map((title, index) => (
+                        <Badge badgeTitle={title} key={index} />
+                      ))}
+                    </div>
+                    <div className={s.descriptions}>
+                      <p className={s.description}>{card.description}</p>
+                    </div>
+                    <div className={s.statuses}>
+                      {card.statuses.map((status, statusIndex) => (
+                        <div className={s.statusItem} key={statusIndex}>
+                          <img src={icon} alt="icon" />
+                          <span className={s.status}>{status}</span>
                         </div>
-                        <div className={s.descriptions}>
-                          <p className={s.description}>
-                            {card.description}
-                          </p>
-                        </div>
-                        <div className={s.statuses}>
-                          {card.statuses.map((status, statusIndex) => (
-                              <div className={s.statusItem} key={statusIndex}>
-                                <img src={icon} alt="icon" />
-                                <span className={s.status}>{status}</span>
-                              </div>
-                          ))}
-                        </div>
-                      </div>
+                      ))}
                     </div>
                   </div>
-              ))}
-            </div>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
-
-        {/* Змінний контент буде рендеритись тут */}
-        <Outlet />
       </div>
+
+      {/* Змінний контент буде рендеритись тут */}
+      <Outlet />
+    </div>
   );
 };
 
