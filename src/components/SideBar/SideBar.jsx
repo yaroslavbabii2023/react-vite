ea985befca28e1cd1cbc27182abf53b654bc2a0a
KILL0real <divannijexpert@gmail.com>
2024-10-24 17:21:31 +0300

    style and centering for SideBar

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,5 +1,29 @@
-const SideBar = () => {
-    return (<aside>Sidebar</aside>)
-}
+import s from "../../components/SideBar/SideBar.module.css";
+import iconTop from "../../assets/icons/hackatons.svg";
+import iconMiddle from "../../assets/icons/profile.svg";
+import iconButton from "../../assets/icons/notifications.svg";
 
-export default SideBar
+const SideBar = () => {
+  return (
+    <aside>
+      <div className={s.container}>
+        <div className={s.wrapper}>
+          <button className={s.button}>
+            {" "}
+            <img src={iconTop} alt="icon" />
+          </button>
+          <button className={s.middleButton}>
+            {" "}
+            <img src={iconMiddle} alt="icon" />
+          </button>
+          <button className={s.button}>
+            {" "}
+            <img src={iconButton} alt="icon" />
+          </button>
+        </div>
+      </div>
+    </aside>
+  );
+};
+
+export default SideBar;
