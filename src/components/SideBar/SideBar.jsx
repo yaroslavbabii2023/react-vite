52f8536cfd4fe38a8e27bfdba59b47ecec68345b
Yaroslav <andrexvich@icloud.com>
2024-10-25 10:55:38 +0300

    create button click

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -3,20 +3,36 @@ import iconTop from "../../assets/icons/hackatons.svg";
 import iconMiddle from "../../assets/icons/profile.svg";
 import iconButton from "../../assets/icons/notifications.svg";
 
+import {useNavigate} from "react-router-dom";
+
 const SideBar = () => {
+  const navigate = useNavigate();
+
+  const mainClick = () => {
+    navigate("/");
+  }
+
+  const profileClick = () => {
+    navigate("/profile");
+  }
+
+  const messageClick = () => {
+    navigate("/message");
+  }
+
   return (
     <aside>
       <div className={s.container}>
         <div className={s.wrapper}>
-          <button className={s.button}>
+          <button onClick={mainClick} className={s.button}>
             {" "}
             <img src={iconTop} alt="icon" />
           </button>
-          <button className={s.middleButton}>
+          <button onClick={profileClick} className={s.middleButton}>
             {" "}
             <img src={iconMiddle} alt="icon" />
           </button>
-          <button className={s.button}>
+          <button onClick={messageClick} className={s.button}>
             {" "}
             <img src={iconButton} alt="icon" />
           </button>
