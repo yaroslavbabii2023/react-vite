5aa89dad5496ac61f915768e18eed1b48b265bab
KILL0real <divannijexpert@gmail.com>
2024-10-25 12:47:15 +0300

    Сохранение перед pull --rebase

--- src/pages/MessagePage/MessagePage.jsx
+++ src/pages/MessagePage/MessagePage.jsx
@@ -1,5 +1,33 @@
+import s from "../../pages/MessagePage/MessagePage.module.css";
+import logo from "../../assets/icons/logo_notifications.svg";
+import image from "../../assets/pictures/image.png";
 const MessagePage = () => {
-    return (<h1>Message Page</h1>)
-}
+  return (
+    <div className={s.container}>
+      <div className={s.wrapper}>
+        <div className={s.header}>
+          <h1 className={s.title}>Приглашения</h1>
+          <a className={s.link} href="#">
+            {" "}
+            <img src={logo} alt="notification" />
+          </a>
+        </div>
+        <div className={s.card}>
+          <div className={cardContainer}>
+            <div className={s.item}>
+              <img src={image} alt="image" />
+              <h2 className={s.titleItem}>
+                ITAM - 1 <br />{" "}
+                <span className={s.subtitleItem}>3 человека</span>
+              </h2>
+            </div>
 
-export default MessagePage
+            <button className={s.button}>принять</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MessagePage;
