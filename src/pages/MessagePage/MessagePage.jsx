cb8935b3b17b5b7f0cbf82a4a265c725e4383bfb
KILL0real <divannijexpert@gmail.com>
2024-10-26 11:42:31 +0300

    Fix sidebar styles, create styles and media query for Message

--- src/pages/MessagePage/MessagePage.jsx
+++ src/pages/MessagePage/MessagePage.jsx
@@ -1,6 +1,9 @@
 import s from "../../pages/MessagePage/MessagePage.module.css";
 import logo from "../../assets/icons/logo_notifications.svg";
 import image from "../../assets/pictures/image.png";
+import Badge from "../../components/Badge/Badge.jsx";
+
+const badgesList = ["Дизайн", "Data Science", "AR"];
 const MessagePage = () => {
   return (
     <div className={s.container}>
@@ -13,16 +16,49 @@ const MessagePage = () => {
           </a>
         </div>
         <div className={s.card}>
-          <div className={cardContainer}>
-            <div className={s.item}>
+          <div className={s.topWrapper}>
+            <div className={s.cardContainer}>
               <img src={image} alt="image" />
-              <h2 className={s.titleItem}>
+              <h2 className={s.titleCard}>
                 ITAM - 1 <br />{" "}
-                <span className={s.subtitleItem}>3 человека</span>
+                <span className={s.subtitleCard}>3 человека</span>
               </h2>
             </div>
 
             <button className={s.button}>принять</button>
+          </div>
+          <div className={s.middleWrapper}>
+            <p className={s.description}>
+              Привет, нам нужен дизайнер, не хочешь к нам <br /> в команду?
+            </p>
+          </div>
+          <div className={s.bottomWrapper}>
+            {badgesList.map((title, index) => (
+              <Badge badgeTitle={title} key={index} />
+            ))}
+          </div>
+        </div>
+        <div className={s.card}>
+          <div className={s.topWrapper}>
+            <div className={s.cardContainer}>
+              <img src={image} alt="image" />
+              <h2 className={s.titleCard}>
+                ITAM - 1 <br />{" "}
+                <span className={s.subtitleCard}>3 человека</span>
+              </h2>
+            </div>
+
+            <button className={s.button}>принять</button>
+          </div>
+          <div className={s.middleWrapper}>
+            <p className={s.description}>
+              Привет, нам нужен дизайнер, не хочешь к нам <br /> в команду?
+            </p>
+          </div>
+          <div className={s.bottomWrapper}>
+            {badgesList.map((title, index) => (
+              <Badge badgeTitle={title} key={index} />
+            ))}
           </div>
         </div>
       </div>
